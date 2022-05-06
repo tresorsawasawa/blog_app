f6403acedb9769e4763ff548d5fd64260fa19c79
tresorsawasawa <tresorsawasawa@gmail.com>
2022-05-06 15:29:08 +0200

    Generate the schema.rb file

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_05_06_120711) do
+ActiveRecord::Schema[7.0].define(version: 2022_05_06_131435) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "comments", force: :cascade do |t|
-    t.string "CreateComments"
     t.bigint "author_id", null: false
     t.bigint "post_id", null: false
     t.text "text"
@@ -26,7 +25,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_05_06_120711) do
   end
 
   create_table "likes", force: :cascade do |t|
-    t.string "CreateLikes"
     t.bigint "author_id", null: false
     t.bigint "post_id", null: false
     t.datetime "created_at", null: false
@@ -36,19 +34,17 @@ ActiveRecord::Schema[7.0].define(version: 2022_05_06_120711) do
   end
 
   create_table "posts", force: :cascade do |t|
-    t.string "CreatePosts"
     t.string "title"
     t.text "text"
     t.integer "comments_counter"
     t.integer "likes_counter"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "user_id", null: false
-    t.index ["user_id"], name: "index_posts_on_user_id"
+    t.bigint "author_id", null: false
+    t.index ["author_id"], name: "index_posts_on_author_id"
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "CreateUsers"
     t.string "name"
     t.string "photo"
     t.text "bio"
@@ -61,5 +57,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_05_06_120711) do
   add_foreign_key "comments", "users", column: "author_id"
   add_foreign_key "likes", "posts"
   add_foreign_key "likes", "users", column: "author_id"
-  add_foreign_key "posts", "users"
+  add_foreign_key "posts", "users", column: "author_id"
 end
