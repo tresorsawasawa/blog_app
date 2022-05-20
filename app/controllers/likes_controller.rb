316379e4a0939c6060541c23d18447496a5195b7
tresorsawasawa <tresorsawasawa@gmail.com>
2022-05-20 16:19:21 +0200

    Add a logic to create a new like

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,4 +1,11 @@
 class LikesController < ApplicationController
   def new
+    @like = Like.new
+  end
+
+  def create
+    post = Post.find(params[:post_id])
+    Like.create(author: current_user, post:)
+    redirect_to use_post_path(current_user, post)
   end
 end
