a3e534d07f4c63409522d449ab2796eecc92641d
tresorsawasawa <tresorsawasawa@gmail.com>
2022-05-21 16:43:38 +0200

    Update db/seeds.rb: add seeders for users

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+first_user = User.create(name: 'Tom', photo: 'https://user-images.githubusercontent.com/83042742/130520357-0eba230c-5e09-4c75-a93c-59f663e94ea6.png', bio: 'Teacher from Mexico.')
+second_user = User.create(name: 'Lilly', photo: 'https://user-images.githubusercontent.com/83042742/130520357-0eba230c-5e09-4c75-a93c-59f663e94ea6.png', bio: 'Teacher from Poland.')
