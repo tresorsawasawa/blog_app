567c52efa623a38d4ded816a91876636710d495c
tresorsawasawa <tresorsawasawa@gmail.com>
2022-05-21 17:18:31 +0200

    Add flash message in the create action for PostsController

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,7 @@
 class PostsController < ApplicationController
   def index
     @user = current_user
+    @posts = @user.posts.includes(:comments)
   end
 
   def new
@@ -9,8 +10,15 @@ class PostsController < ApplicationController
   end
 
   def create
-    Post.create(post_params)
-    redirect_to { user_posts(current_user) }
+    @post = Post.new(post_params)
+
+    if @post.save
+      flash[:notice] = 'New post created successfully!'
+      redirect_to { user_posts(current_user) }
+    else
+      flash[:alert] = 'Can not add a new post.'
+      redirect_to { new_user_post(current_user) }
+    end
   end
 
   def show
