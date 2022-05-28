fcbf60eea1cf025da00217a21de320769545c2ad
tresorsawasawa <tresorsawasawa@gmail.com>
2022-05-28 02:21:25 +0200

    Fix a bug

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,12 +4,11 @@ class Ability
   def initialize(user)
     can :read, :all if user.nil?
 
-    if user.admin?
-      can :manage, :all
-    else
-      can :read, :all
-      can :manage, Post, author_id: user.id
-      can :manage, Comment, author_id: user.id
-    end
+    return if user.nil?
+
+    can :manage, :all if user.role == 'admin'
+    can :read, :all if user.role == 'user'
+    can :manage, Post, author_id: user.id
+    can :manage, Comment, author_id: user.id
   end
 end
