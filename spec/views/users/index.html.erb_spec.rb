d6c7e82332e6edb693a28b17088a438ec8c343c6
tresorsawasawa <tresorsawasawa@gmail.com>
2022-05-28 15:30:47 +0200

    Add unit tests on the Login page, User post index page and user show page

--- spec/views/users/index.html.erb_spec.rb
+++ spec/views/users/index.html.erb_spec.rb
@@ -4,12 +4,12 @@ RSpec.describe 'user_index_path', type: :system do
   describe 'User_index_view' do
     before(:each) do
       @users = [User.create!(name: 'Tresor', photo: '#photo_amy', bio: 'bio', email: 'tresor@test.com', password: 'pswd123', posts_counter: 0),
-                User.create!(name: 'Amy', photo: '#photo_amy', bio: 'bio', email: 'amy@test.com', password: 'pswd123', posts_counter: 10),
-                User.create!(name: 'Test', bio: 'bio', email: 'test@test.com', password: 'password', posts_counter: 21)]
+                User.create!(name: 'James', photo: '#photo_james', bio: 'bio', email: 'james@test.com', password: 'pswd123', posts_counter: 10),
+                User.create!(name: 'Joe', bio: 'bio', email: 'joekam@test.com', password: 'password', posts_counter: 21)]
 
       visit new_user_session_path
 
-      fill_in 'user_email',	with: 'Test@test.com'
+      fill_in 'user_email',	with: 'james@test.com'
       fill_in 'user_password',	with: 'password'
       click_button 'Log in'
     end
